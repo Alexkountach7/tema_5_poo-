5253b8aab979611b68dc31b8493ef66459b4294e
A614\1DAW <alexkountach7@gmail.com>
2026-01-15 17:21:38 +0100

    ejercicios_perros_poo

---
+++ ejercicios_poo_inical/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        perro perro1 = new perro("caniche","permanente",1,"verde");
+        perro perro2 = new perro();
+        perro1.imprimirficha();
+
+
+
+
+
+
+
+    }
+}
---
+++ ejercicios_poo_inical/src/main/java/org/example/perro.java
@@ -0,0 +1,51 @@
+package org.example;
+
+public class perro {
+    private String raza;
+    private String pelo;
+    private int peso;
+    private int patas;
+    private String color;
+
+    public perro(String raza, String pelo, int peso, String color) {
+
+        this.raza = raza;
+        this.pelo = pelo;
+        this.peso = peso;
+        this.patas = patas;
+        this.color = color;
+    }
+
+    public perro() {
+
+    }
+
+    public void imprimirficha() {
+        System.out.println("raza: " + raza);
+        System.out.println("pelo: " + pelo);
+        System.out.println("peso: " + peso);
+        System.out.println("patas: " + patas);
+        System.out.println("color" + color);
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza() {
+        raza = raza;
+    }
+
+    public void setRaza(String raza) {
+
+        this.raza = raza;
+    }
+
+    public String getPelo() {
+        return pelo;
+    }
+    public void setPelo(String pelo){
+        pelo = pelo;
+    }
+    public  void setPelo
+}
