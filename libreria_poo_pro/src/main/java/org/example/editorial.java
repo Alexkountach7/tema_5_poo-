fdf9506600c77c9a0ed4584b07bca077cb12c3f7
A614\1DAW <alexkountach7@gmail.com>
2026-01-29 16:30:26 +0100

    ejercicio libro programacion poo terminado

---
+++ libreria_poo_pro/src/main/java/org/example/editorial.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class editorial {
+    private String nombre;
+    private String pais;
+    private ArrayList<libro> librospublicados;
+
+    public editorial(String nombre, String pais, ArrayList<libro> librospublicados) {
+        this.nombre = nombre;
+        this.pais = pais;
+        this.librospublicados = librospublicados;
+
+    }
+
+    public void añadirlibro(libro uno) {
+        librospublicados.add(uno);
+
+    }
+
+    public void borrarlibro(libro uno) {
+        librospublicados.remove(uno);
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getPais(String pais) {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    public ArrayList<libro> getLibrospublicados() {
+        return librospublicados;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + "del pais: " +pais+ " )";
+    }
+
+}
---
+++ libreria_poo_pro/src/main/java/org/example/libreriaApp.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class libreriaApp {
+    static void main() {
+
+        estudiante estudiante1 = new estudiante("alex", "45585545d", "1daw");
+        estudiante estudiante2 = new estudiante("manuel", "54845614d", "1daw");
+        estudiante estudiante3 = new estudiante("alvaro", "854845454d", "1daw");
+
+
+    }
+}
